311bdbdebd343ea38623493063ef0cb0e8382189
sinansaricayir <sinansaricayir@gmail.com>
2023-05-25 18:06:47 +0300

    Update Navbar.sx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -97,7 +97,7 @@ const Navbar = () => {
           className={
             nav
               ? "md:hidden fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500"
-              : "fixed left-[-100%] top-0 h-screen bg-[#ecf0f3] p-10 ease-in duration-500"
+              : "fixed left-[-140%] top-0 h-screen bg-[#ecf0f3] p-10 ease-in duration-500"
           }
         >
           <div className="">
